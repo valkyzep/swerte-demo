5a2efb2df1b4957ee965b6a4f1a8b558f28c42af
valkyzep <qwertyasdf.ron10@gmail.com>
2025-05-29 15:36:39 +0800

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src')
-    }
+      "@": resolve(__dirname, "src"),
+    },
   },
+  base: "/swerte-demo",
   server: {
-    port: 5173
-  }
-})
+    port: 5173,
+  },
+});
